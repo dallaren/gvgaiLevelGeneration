e4c67aa5d3250a383ca100b607e5bf3d341283e6
Jonas <dallaren@gmail.com>
2017-03-28 11:50:31 +0200

    stuff

--- src/levelGenerators/jaspGeneticLevelGenerator/Individual.java
+++ src/levelGenerators/jaspGeneticLevelGenerator/Individual.java
@@ -2,16 +2,34 @@ package levelGenerators.jaspGeneticLevelGenerator;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-public class Individual {
+import java.util.Comparator;
+
+import static levelGenerators.jaspGeneticLevelGenerator.Shared.*;
+
+public class Individual implements Comparator<Individual> {
     private char[][] level;
 
     public Individual() {
+        level = new char[levelHeight][levelWidth];
+    }
 
+    public void initializeRandom() {
+        throw new NotImplementedException();
     }
 
     //randomly mutate a single tile in the level
     public void mutate() {
+        int row = random.nextInt(levelHeight);
+        int col = random.nextInt(levelWidth);
+        char c = getRandomCharFromLevelMapping();
 
+        level[col][row] = c;
+    }
+
+    private char getRandomCharFromLevelMapping() {
+        Character[] mappingChars = (Character[]) game.getLevelMapping().keySet().toArray();
+        int c = random.nextInt(mappingChars.length);
+        return mappingChars[c];
     }
 
     public Iterable<Individual> crossover(Individual partner) {
@@ -22,4 +40,10 @@ public class Individual {
         throw new NotImplementedException();
     }
 
+    public char[][] getLevel() { return level; }
+
+    @Override
+    public int compare(Individual o1, Individual o2) {
+        throw new NotImplementedException();
+    }
 }
